48d15773f48e5f40daa036e6736f21a355fcaa97
shubhamv007 <shubhamvaishnav8989@gmail.com>
2024-07-01 19:37:32 +0530

    Added Icon in pdf file

--- main.py
+++ main.py
@@ -33,21 +33,34 @@ class PdfReport:
     def __init__(self, filenames):
         self.filename = filenames
 
-    def generate(self, flatmate_1, flatemate_2, bill):
+    def generate(self, flatmate_1, flatmate_2, bill):
+
+        flatmate_1_pay = str(round(flatmate_1.pays(bill, flatmate_2),2))
+        flatmate_2_pay = str(round(flatmate_2.pays(bill, flatmate_1),2))
+
         pdf = FPDF( orientation = "P", unit = "pt", format = 'A4')
         pdf.add_page()
 
+        # Add Icon
+        pdf.image("house.png", w=50, h=50)
+
         # Insert title
         pdf.set_font(family = "Times", size = 24, style = 'B')
-        pdf.cell(w = 0, h=80, txt = "Flatmate Bill", border = 1, align= 'C', ln=1)
+        pdf.cell(w = 0, h=80, txt = "Flatmate Bill", border = 0, align= 'C', ln=1)
 
         # Insert Period label and value
-        pdf.cell(w = 100, h=40, txt = "Period", border = 1)
-        pdf.cell(w = 130, h=40, txt = bill.period, border = 1)
+        pdf.set_font(family = "Times", size = "15", style = 'B')
+        pdf.cell(w = 100, h=40, txt = "Period", border = 0)
+        pdf.cell(w = 130, h=40, txt = bill.period, border = 0)
         
         # Insert name and due amount of the first flatmate
-        pdf.cell(w = 100, h=40, txt = flatmate_1.name, border = 1)
-        pdf.cell(w = 130, h=40, txt = str(flatmate_1.pays(bill, flatemate_2)), border = 1)
+        pdf.set_font(family = "Times", size = "12")
+        pdf.cell(w = 100, h=25, txt = flatmate_1.name, border = 0)
+        pdf.cell(w = 130, h=25, txt = flatmate_1_pay, border = 0)
+
+        # Insert name and due amount of the second flatmate
+        pdf.cell(w = 100, h=25, txt = flatmate_2.name, border = 0)
+        pdf.cell(w = 130, h=25, txt = flatmate_2_pay, border = 0)
 
         # Inserted Author
         pdf.set_author('SHUBHAM VAISHNAV')
